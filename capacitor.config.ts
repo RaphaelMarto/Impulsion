5b2da83d31dbda42aa3c05d7527976f8f40b31ac
raphaelmarto <r.marto@students.ephec.be>
2023-03-06 15:19:58 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.tfe',
+  appName: 'tfe',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
