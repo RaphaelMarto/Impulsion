4bf028908b775cced78cec645fc97c1203b98a64
RaphaelMarto <92374387+RaphaelMarto@users.noreply.github.com>
2024-01-19 18:57:42 +0100

    [Feature] Recherche par location

--- src/app/Page/tabs-list/tab2/tab2.module.ts
+++ src/app/Page/tabs-list/tab2/tab2.module.ts
@@ -3,13 +3,21 @@ import { HeaderModule } from './../../../components/header/header.module';
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
 @NgModule({
-  imports: [IonicModule, CommonModule, FormsModule, Tab2PageRoutingModule, HeaderModule, HttpClientModule],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    Tab2PageRoutingModule,
+    HeaderModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   declarations: [Tab2Page],
 })
 export class Tab2PageModule {}
--- src/app/Page/tabs-list/tab2/tab2.page.ts
+++ src/app/Page/tabs-list/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, of, take } from 'rxjs';
 import { config } from 'src/app/config/configuration';
@@ -13,10 +14,17 @@ export class Tab2Page implements OnInit {
   public buttonFocus: string = 'artiste';
   private options = { withCredentials: true };
   public dataObserv!: Observable<any[]>;
+  public country: any = [];
+  public city: any = [];
+  locationForm!: FormGroup;
   @ViewChild('searchBar') searchBar: any;
+  public url = config.API_URL + '/user/proxy-image?url=';
 
-  constructor(private http: HttpClient, private router: Router) {
-    
+  constructor(private http: HttpClient, private router: Router, private fb: FormBuilder) {
+    this.locationForm = this.fb.group({
+      country: [null, Validators.required],
+      city: [{ value: null, disabled: true }, Validators.required],
+    });
   }
 
   async ngOnInit() {}
@@ -40,22 +48,64 @@ export class Tab2Page implements OnInit {
     }
   }
 
-  InputArtist(query: any) {
-    this.http.get(config.API_URL + '/user/all/' + query, this.options).pipe(take(1)).subscribe((res:any) => {
-      this.dataObserv = of(res);
-    });
+  InputArtist(query: string) {
+    this.http
+      .get(config.API_URL + '/user/all/' + query, this.options)
+      .pipe(take(1))
+      .subscribe((res: any) => {
+        this.dataObserv = of(res);
+      });
   }
 
-  InputMusique(query: any) {
-    this.http.get(config.API_URL + '/music/all/' + query, this.options).pipe(take(1)).subscribe((res:any) => {
-      this.dataObserv = of(res);
-    });
+  InputMusique(query: string) {
+    this.http
+      .get(config.API_URL + '/music/all/' + query, this.options)
+      .pipe(take(1))
+      .subscribe((res: any) => {
+        this.dataObserv = of(res);
+      });
   }
 
-  InputInstrument(query: any) {
-    this.http.get(config.API_URL + '/music/instrument/all/' + query, this.options).pipe(take(1)).subscribe((res:any) => {
-      this.dataObserv = of(res);
-    });
+  InputInstrument(query: string) {
+    this.http
+      .get(config.API_URL + '/music/instrument/all/' + query, this.options)
+      .pipe(take(1))
+      .subscribe((res: any) => {
+        this.dataObserv = of(res);
+      });
+  }
+
+  InputLocation() {
+    if (this.locationForm.valid) {
+      const selectedCity = this.locationForm.get('city')?.value;
+      this.http
+        .get(config.API_URL + '/user/location/' + selectedCity, this.options)
+        .pipe(take(1))
+        .subscribe((res: any) => {
+          this.dataObserv = of(res);
+        });
+    }
+  }
+
+  getCountry() {
+    this.http
+      .get(config.API_URL + '/user/country/', this.options)
+      .pipe(take(1))
+      .subscribe((res: any) => {
+        this.country = res;
+      });
+  }
+
+  onCountryChange(event: any) {
+    if (!!this.locationForm.get('country')?.value) {
+      this.locationForm.get('city')?.enable();
+      this.http
+        .get(config.API_URL + '/user/city/' + event.target.value, this.options)
+        .pipe(take(1))
+        .subscribe((res: any) => {
+          this.city = res;
+        });
+    }
   }
 
   getProfile(id: string) {
@@ -66,5 +116,13 @@ export class Tab2Page implements OnInit {
     this.buttonFocus = name;
     this.searchBar.value = '';
     this.dataObserv = of([]);
+    if(name == 'Localiter'){
+      this.locationForm.get('city')?.setValue(null);
+      this.locationForm.get('city')?.disable();
+      this.locationForm.get('country')?.setValue(null);
+      this.city = null;
+      this.country = null;
+      this.getCountry();
+    }
   }
 }
